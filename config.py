2d6dcff465c5ff3e2baa947a323117f080461ad4
zenaku17psp <zenaku16psp@outlook.com>
2026-02-16 18:40:31 +0630

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-import re
+byimport re
 import os
 from os import getenv
 
@@ -42,7 +42,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/rm-105/infinity",
+    "https://github.com/zenaku17psp/smm",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
@@ -53,11 +53,10 @@ GIT_TOKEN = getenv(
 SUPPORT_CHANNEL_LINK = getenv("SUPPORT_CHANNEL_LINK", "@JBmusic_myanmar")
 SUPPORT_CHAT_LINK = getenv("SUPPORT_CHAT_LINK", "@JBmusic_myanmar2002")
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/cupid153")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/infinitylovegp")
-
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/JBmusic_myanmar")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/JBmusic_myanmar2002")
 # Set this to True if you want the assistant to automatically leave chats after an interval
-AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
+AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 
 # Auto Gcast/Broadcast Handler (True = broadcast on , False = broadcast off During Hosting, Dont Do anything here.)
 AUTO_GCAST = os.getenv("AUTO_GCAST")
@@ -100,22 +99,23 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/a5b0jd.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/0gdu2w.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://files.catbox.moe/a5b0jd.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/0gdu2w.jpg"
 )
-PLAYLIST_IMG_URL = "https://files.catbox.moe/a5b0jd.jpg"
-STATS_IMG_URL = "https://files.catbox.moe/a5b0jd.jpg"
-JOIN_IMG_URL = "https://files.catbox.moe/a5b0jd.jpg"
-TELEGRAM_AUDIO_URL = "https://files.catbox.moe/a5b0jd.jpg"
+PLAYLIST_IMG_URL = "https://files.catbox.moe/0gdu2w.jpg"
+STATS_IMG_URL = "https://files.catbox.moe/0gdu2w.jpg"
+JOIN_IMG_URL = "https://files.catbox.moe/61mg5q.jpg"
+TELEGRAM_AUDIO_URL = "https://files.catbox.moe/0gdu2w.jpg"
 TELEGRAM_VIDEO_URL = "https://files.catbox.moe/0gdu2w.jpg"
-STREAM_IMG_URL = "https://files.catbox.moe/a5b0jd.jpg"
+STREAM_IMG_URL = "https://files.catbox.moe/tyeotp.jpg"
 SOUNCLOUD_IMG_URL = "https://files.catbox.moe/tyeotp.jpg"
-YOUTUBE_IMG_URL = "https://files.catbox.moe/a5b0jd.jpg"
+YOUTUBE_IMG_URL = "https://files.catbox.moe/0gdu2w.jpg"
 SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/tyeotp.jpg"
 SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/tyeotp.jpg"
 SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/tyeotp.jpg"
+
 
 
 def time_to_seconds(time):
