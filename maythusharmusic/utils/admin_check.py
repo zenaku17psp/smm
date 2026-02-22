bc70bdb705511d3428b9fd79fa76a8f938d3804e
zenaku17psp <zenaku16psp@outlook.com>
2026-02-22 10:15:24 +0630

    Update admin_check.py

--- maythusharmusic/utils/admin_check.py
+++ maythusharmusic/utils/admin_check.py
@@ -11,7 +11,7 @@ async def admin_check(message: Message) -> bool:
 
     if message.from_user.id in [
         777000,  # Telegram Service Notifications
-        6534087733,  # GroupwcgbrandedBot
+        5821905026,  # GroupwcgbrandedBot
     ]:
         return True
 
